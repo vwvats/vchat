76bb94b66c7cbec1969c68112c9e4afdb2f7b0c8
VW Vats <vwvats@gmail.com>
2022-02-21 23:07:28 +0530

    instantiated the chat client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,9 @@
 import { ChannelContainer, ChannelListContainer } from "./components";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie/es6";
+
+// const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
